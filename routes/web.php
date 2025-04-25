c5ad5604c4de8e56bbf7f3969f23bff1e633f134
Gon161 <nerialvareze@gmail.com>
2025-04-24 20:14:04 -0600

    metodos get/post

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
+use Illuminate\Http\Request;
 
-Route::get('/', [ProductoController::class, 'index']);
+//Route::get('/', [ProductoController::class, 'index']);
+
+
+// GET con parámetro en la URL
+Route::get('/{nombre}/{numero}', function ($nombre,$numero) {
+    return response()->json(['mensaje' => "Hola $nombre, numero de la suerte es $numero"]);
+});
+
+// POST con parámetro en el body
+Route::post('/saludo', function (Request $request) {
+    $nombre = $request->nombre;
+    $edad = $request->edad;
+    return response()->json(['mensaje' => "Hola $nombre, tienes $edad años."]);
+});
